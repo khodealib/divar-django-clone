3596abb7209c22706dc26db39bd6f72b703d91a0
Ali Bagheri <khodealib@gmail.com>
2024-06-27 06:26:17 +0330

    Add test for user model

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,39 @@
+from django.db import IntegrityError
 from django.test import TestCase
 
+from accounts.models import User
+
+
 # Create your tests here.
+class UserModelTest(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(
+            email="test@example.com",
+            password="test123",
+            first_name="Test",
+            last_name="Test",
+        )
+        self.superuser = User.objects.create_superuser(
+            email="admin@example.com",
+            password="admin123",
+            first_name="Admin",
+            last_name="Admin",
+        )
+
+    def test_user_creation(self):
+        self.assertTrue(self.user.is_authenticated)
+        self.assertTrue(self.superuser.is_staff)
+        self.assertTrue(self.superuser.is_superuser)
+
+    def test_user_full_name(self):
+        self.assertEqual(self.user.full_name, "Test Test")
+        self.assertEqual(self.superuser.full_name, "Admin Admin")
+
+    def test_user_email_uniqueness(self):
+        with self.assertRaises(IntegrityError):
+            User.objects.create_user(
+                email="test@example.com",
+                password="test123",
+                first_name="Test",
+                last_name="Test",
+            )
