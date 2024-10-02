1a5fe61b925e6ceea1b3099da7d4eb971dc02181
Ali Bagheri <khodealib@gmail.com>
2024-10-02 17:36:39 +0330

    chore: :sparkles: add locations app
    implement province, city, District, location models

---
+++ locations/models.py
@@ -0,0 +1,64 @@
+from django.db import models
+from django.utils.translation import ugettext_lazy as _
+
+from utilities.base_model import BaseModel
+
+
+class Province(BaseModel):
+    name = models.CharField(max_length=50, verbose_name=_("name"))
+    slug = models.SlugField(max_length=50, allow_unicode="True", verbose_name=_("slug"))
+
+    def __str__(self):
+        return f"{self.name}"
+
+    class Meta:
+        verbose_name = "province"
+        verbose_name_plural = "provinces"
+
+
+class City(BaseModel):
+    name = models.CharField(max_length=50, verbose_name=_("name"))
+    slug = models.SlugField(max_length=50, allow_unicode="True", unique=True, blank=True, verbose_name=_("slug"))
+    state = models.ForeignKey(Province, related_name="cities", on_delete=models.CASCADE, verbose_name=_("state"))
+
+    def __str__(self):
+        return f"{self.name}"
+
+    class Meta:
+        verbose_name = _("city")
+        verbose_name_plural = _("cities")
+
+    def get_absolute_url(self):
+        from django.urls import reverse
+
+        return reverse("advertisement-list", args=[self.name])
+
+
+class District(BaseModel):
+    name = models.CharField(max_length=50, verbose_name=_("name"))
+    slug = models.SlugField(max_length=50, allow_unicode="True", verbose_name=_("slug"))
+    city = models.ForeignKey(City, related_name="districts", on_delete=models.CASCADE, verbose_name=_("city"))
+
+    def __str__(self):
+        return f"{self.name}"
+
+    class Meta:
+        verbose_name = _("district")
+        verbose_name_plural = _("districts")
+
+
+class Location(BaseModel):
+    """
+    This model represents location 🚩 of published advertisement by User
+    """
+
+    province = models.ForeignKey(Province, on_delete=models.CASCADE, related_name=_("province"))
+    city = models.ForeignKey(City, on_delete=models.CASCADE, related_name=_("city"))
+    district = models.ForeignKey(District, on_delete=models.CASCADE, blank=True, null=True, verbose_name=_("district"))
+
+    def __str__(self):
+        return f"{self.city} < {self.province}"
+
+    class Meta:
+        verbose_name = _("location")
+        verbose_name_plural = _("locations")
---
+++ utilities/username.py
@@ -0,0 +1,10 @@
+import math
+import secrets
+
+
+def generate_random_string():
+    """
+    Create a random username
+    """
+    username = secrets.token_urlsafe(math.floor(32 / 1.3))
+    return username
